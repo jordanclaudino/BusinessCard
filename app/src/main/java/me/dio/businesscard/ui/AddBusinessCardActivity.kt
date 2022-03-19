580bc03d152d16b9ab663d25fe41491af27b2cb7
Jordan Claudino <jordanclaudino@gmail.com>
2022-03-19 11:55:39 -0300

    third commit with dropdown menu color implementation

--- app/src/main/java/me/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,8 +2,10 @@ package me.dio.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContentProviderCompat.requireContext
 import me.dio.businesscard.App
 import me.dio.businesscard.R
 import me.dio.businesscard.data.BusinessCard
@@ -22,7 +24,13 @@ class AddBusinessCardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         insertListener()
+
+        val colors = resources.getStringArray(R.array.colors)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, colors)
+        binding.actvColor.setAdapter(arrayAdapter)
     }
+
+
 
     private fun insertListener(){
         binding.btClosedButton.setOnClickListener{
@@ -30,12 +38,26 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener{
+            val color = binding.tilColor.editText?.text.toString()
+
+            val tradutor = when(color){
+                "Branco" -> "#FFFFFF"
+                "Azul" -> "#6887F9"
+                "Verde" -> "#89F968"
+                "Vermelho" -> "#F93939"
+                "Cinza" -> "#C1AFAF"
+                "Roxo" -> "#BB42CC"
+                "Amarelo" -> "#E7EA2D"
+                "Laranja" -> "#F9AD2A"
+                else -> "#FFFFFF"
+            }
+
             val businessCard = BusinessCard(
                 name = binding.tilName.editText?.text.toString(),
                 cellphone = binding.tilCellphone.editText?.text.toString(),
                 mail = binding.tilMail.editText?.text.toString(),
                 company = binding.tilCompany.editText?.text.toString(),
-                background = binding.tilColor.editText?.text.toString()
+                background = tradutor
             )
             mainViewModel.insert(businessCard)
             Toast.makeText(this, R.string.label_show_sucess, Toast.LENGTH_SHORT).show()
